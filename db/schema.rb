4a0359015988593423a7096e8d27737f6677fcf8
Russell Schmidt <russell.schmidt@me.com>
2017-01-04 09:52:21 -0800

    add uuid field to purchases model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103235813) do
+ActiveRecord::Schema.define(version: 20170104175117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170103235813) do
     t.integer  "product_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "uuid"
   end
 
 end
