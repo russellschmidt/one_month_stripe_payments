cfb166c8df24fb2495f6513798a4bffa6b7dccac
Russell Schmidt <russell.schmidt@me.com>
2017-01-04 23:16:24 -0800

    got email working on dev machine for emailed receipts

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,6 +1,14 @@
 class Purchase < ApplicationRecord
+	after_create :email_purchaser
+
 	def to_param
 		# overriding the default to_param method that returns the :id field
 		uuid
 	end
+
+	private
+	def email_purchaser
+		PurchaseMailer.purchase_receipt(self).deliver
+	end
+
 end
