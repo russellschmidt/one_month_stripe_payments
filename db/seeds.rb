3b3d5875d84e7dd37061497b9a64b1f8e73f0e6f
Russell Schmidt <russell.schmidt@me.com>
2017-01-06 20:24:57 -0800

    seeded the db for Product and added unless validation for AdminUser

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+unless AdminUser.find_by email: 'admin@example.com'
+	AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+end
 
 growth_hacking = Product.create(
 	title: "Growth Hacking",
