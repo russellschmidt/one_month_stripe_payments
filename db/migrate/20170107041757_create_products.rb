93d7621be929060c6443ce6fe5b6a7da2ddd5756
Russell Schmidt <russell.schmidt@me.com>
2017-01-06 20:20:27 -0800

    add migration for Product

--- db/migrate/20170107041757_create_products.rb
+++ db/migrate/20170107041757_create_products.rb
@@ -1,7 +1,12 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
-
+    	t.string 	:title
+    	t.string 	:subtitle
+    	t.string 	:author
+    	t.text 		:description
+    	t.string	:sku
+    	t.decimal	:price
       t.timestamps
     end
   end
