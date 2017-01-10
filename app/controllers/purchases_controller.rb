814aa5992dbce4c592669e9d9af4cdae4ae393f5
Russell Schmidt <russell.schmidt@me.com>
2017-01-09 16:48:01 -0800

    overcame a nasty pg problem and deal with issue of crashings views when no item in Purchases

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,8 @@
 class PurchasesController < ApplicationController
 	def show 
 		@purchase = Purchase.find_by_uuid(params[:id])
-		@product = Product.find(@purchase.product_id)
+		if @purchase.present?
+			@product = Product.find(@purchase.product_id)
+		end
 	end
 end
